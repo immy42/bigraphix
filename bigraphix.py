a6b9f9cc0bb901a6d5dc12e40c5b2404f4bd9b44
RG <67166974+immy42@users.noreply.github.com>
2023-03-28 03:04:23 +0100

    Add files via upload
    v0.0.1

---
+++ bigraphix.py
@@ -0,0 +1,7 @@
+def draw_pair(trading_pair):
+    import grafix as gx
+    # Use self-made grafix api to display binance data.
+    import selenium
+    gx.draw_candle_chart(640,480)
+
+draw_pair("A")
